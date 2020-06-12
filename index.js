d91d0f0921d3a34513a767699be0ad67295b450b
Leo Gaskin <leo.gaskin@brg-feldkirchen.at>
2020-06-12 22:17:08 +0200

    Adapt for file inclusion trick

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
 const core = require('@actions/core');
 const exec = require('@actions/exec');
 const fs = require('fs')
+
+const local_file_name = '__github-action-check-elisp.el';
 const elisp = fs.readFileSync(__dirname + '/elisp-check.el', 'utf-8')
 
-async function run() {
+async function main() {
   try {
+    // Setup local elisp file
+    fs.writeFileSync(local_file_name, elisp);
+
+    // Get check name and execute
     const check = core.getInput('checks');
-
-    // Test file inclusion
-    console.log(elisp);
-
-    core.debug((new Date()).toTimeString())
-    await exec.exec('emacs', [ '--no-site-file', '--batch', '--eval', `(message "${check}")` ])
-    await exec.exec('ls')
-    core.debug((new Date()).toTimeString())
-
-    core.setOutput('time', new Date().toTimeString());
+    await exec.exec(
+      'emacs',
+      [
+        '--no-site-file',
+        '--batch',
+        '--load', local_file_name,
+        '--eval', `(check-elisp-run '${check})`
+      ]
+    );
   }
   catch (error) {
     core.setFailed(error.message);
   }
 }
 
-run()
+main()
