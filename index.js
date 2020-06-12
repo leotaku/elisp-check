715664c743936eb19342fce99235041ee4332f13
Leo Gaskin <leo.gaskin@brg-feldkirchen.at>
2020-06-12 20:51:24 +0200

    Use single check

--- index.js
+++ index.js
@@ -3,12 +3,10 @@ const exec = require('@actions/exec');
 
 async function run() {
   try {
-    const list = core.getInput('checks');
-    const array = Array.from(list);
-    const spaced = array.join(" ");
+    const check = core.getInput('checks');
 
     core.debug((new Date()).toTimeString())
-    await exec.exec('emacs', [ '--no-site-file', '--batch', '--eval', '(message "foo")' ])
+    await exec.exec('emacs', [ '--no-site-file', '--batch', '--eval', `(message "${check}")` ])
     await exec.exec('ls')
     core.debug((new Date()).toTimeString())
 
