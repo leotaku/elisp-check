731a3a02e8090cc18646984e23446719997c8e21
Leo Gaskin <leo.gaskin@le0.gs>
2021-09-26 11:10:03 +0200

    Add support for the NonGNU ELPA repository

--- elisp-check.el
+++ elisp-check.el
@@ -168,6 +168,7 @@ Only returns buffers for files that match PREFIX."
   "Setup package.el and install packages for the given CHECK."
   (package-initialize)
   (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+  (add-to-list 'package-archives '("nongnu" . "https://elpa.nongnu.org/nongnu/"))
   (package-refresh-contents)
   (elisp-check--install-packages (elisp-check--get-props check :package)))
 
